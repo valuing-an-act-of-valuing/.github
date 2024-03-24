50f867bdb0d523ff0b5b346a016302248c2875e4
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2024-03-24 18:42:42 +0900

    value

--- profile/js/localStorage.js
+++ profile/js/localStorage.js
@@ -5,14 +5,17 @@ document.addEventListener('readystatechange', event => {
         const yourValue = document.querySelector('h1 strong')
         const yourText = document.querySelector('#value')
         const yourName = document.querySelector('h1 small')
+
         if (localStorage.getItem("yourName") && localStorage.getItem("yourEmail")) {
             yourName.textContent = localStorage.getItem("yourName")
         }
 
         if (localStorage.getItem("yourValue") && localStorage.getItem("yourLan") && localStorage.getItem("valuText")) {
             yourValue.textContent = localStorage.getItem("yourValue")
-            yourText.innerHTML = localStorage.getItem("valuText").replace(/\n/g,'<br>');
+            yourText.innerHTML = localStorage.getItem("valuText").replace(/\n/g, '<br>');
             yourText.className = `${localStorage.getItem("yourLan")}_app`;
+        } else {
+            
         }
     }
 }, false)
