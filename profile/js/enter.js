c44ee8fed1b180bb0d49488db4157fdcb2de3bfc
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2024-03-24 10:39:24 +0900

    velue

--- profile/js/enter.js
+++ profile/js/enter.js
@@ -2,16 +2,16 @@
 
 document.addEventListener('readystatechange', event => {
     if (event.target.readyState === 'interactive') {
-        const yourName = localStorage.getItem("yourName");
-        const name = document.querySelector('#yourName');
+        const yourName = localStorage.getItem("yourName")
+        const name = document.querySelector('#yourName')
         name.value = yourName;
 
-        const yourEmail = localStorage.getItem("yourEmail");
-        const email = document.querySelector('#yourEmail');
+        const yourEmail = localStorage.getItem("yourEmail")
+        const email = document.querySelector('#yourEmail')
         email.value = yourEmail;
 
         if (localStorage.getItem("yourName") && localStorage.getItem("yourEmail")) {
-            location.replace('online/');
+            location.replace('online/')
         }
     } else if (event.target.readyState === 'complete') {
         // フォームの名前からそのフォームへの参照を取得する
@@ -19,13 +19,13 @@ document.addEventListener('readystatechange', event => {
 
         // 'submit' イベントのハンドラーを追加
         form.addEventListener("submit", (event) => {
-            event.preventDefault();
-            let formData = new FormData(form);
+            event.preventDefault()
+            let formData = new FormData(form)
             // key/value ペアをリストします
             for (let [name, value] of formData) {
-                localStorage.setItem(name, value);
+                localStorage.setItem(name, value)
             }
-            location.replace('online/');
+            location.replace('online/')
         });
     }
 });
